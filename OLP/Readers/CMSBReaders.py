df7c02660b9566c9e30cd276bda9d0ae4c189e7e
i405817461 <likeliurui@qq.com>
2015-07-22 21:03:22 +0800

    /

--- OLP/Readers/CMSBReaders.py
+++ OLP/Readers/CMSBReaders.py
@@ -84,3 +84,39 @@ class CMSBTransReader(Reader):
                         trans[j].append(val)
 
         return title2index, transs
+
+
+class ContactReader(Reader):
+    '''
+    the Reader can read product contact table
+    '''
+
+    # Notice!!!
+    # the class variable will be defined by the imported config file
+    signedProdCode = 'signedProdCode'
+
+    def __init__(self, filenames, primKey):
+        self.filenames = filenames
+        self.primKey = primKey
+
+    def read(self):
+        wholeTable = {}
+        for tablePath in self.filenames:
+            tableObject = open(tablePath)
+            title2index = {}
+            titleList = tableObject.readline().strip().split('\t')
+            for index, title in enumerate(titleList):
+                title2index[title] = index
+            for index,value in enumerate(title2index):
+                print index,value
+
+            for tableRecord in tableObject:
+                tableList = tableRecord.strip().split('\t')
+                tableRecordKey = tableList[title2index[self.primKey]]
+                tableRecordValue = tableList[title2index[self.signedProdCode]]
+
+                if tableRecordKey in wholeTable:
+                    wholeTable[tableRecordKey].append(tableRecordValue)
+                else:
+                    wholeTable[tableRecordKey] = [tableRecordValue,]
+        return wholeTable
