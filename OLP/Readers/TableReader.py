2ae1e3394c0e25207e61d46c9192d4148f26e84e
i405817461 <likeliurui@qq.com>
2015-07-22 20:40:58 +0800

    fix ContactReader

--- OLP/Readers/TableReader.py
+++ OLP/Readers/TableReader.py
@@ -7,38 +7,39 @@ class ContactReader(Reader):
     the Reader can read product contact table
     '''
 
+    # Notice!!!
+    # the class variable will be defined by the imported config file
+    signedProdCode = 'signedProdCode'
+
     def __init__(self, filenames, primKey):
         self.filenames = filenames
         self.primKey = primKey
 
     def read(self):
-        wholeTable = []
-        for tablePath in tablePathList:
-
-
-            table = []
+        wholeTable = {}
+        for tablePath in self.filenames:
             tableObject = open(tablePath)
-            tableKey = []
-            lineNum = 0
-            for line in tableObject:
-                if lineNum == 0:
-                    tableKey = line.strip().split('\t')
-                    lineNum = 1
+            title2index = {}
+            titleList = tableObject.readline().strip().split('\t')
+            for index, title in enumerate(titleList):
+                title2index[title] = index
+            for index,value in enumerate(title2index):
+                print index,value
+
+            for tableRecord in tableObject:
+                tableList = tableRecord.strip().split('\t')
+                tableRecordKey = tableList[title2index[self.primKey]]
+                tableRecordValue = tableList[title2index[self.signedProdCode]]
+
+                if tableRecordKey in wholeTable:
+                    wholeTable[tableRecordKey].append(tableRecordValue)
                 else:
-                    print tableKey[0]
-                    tableValue = line.strip().split('\t')
-                    tableDict = {}
-                    for elementNum in range(len(tableKey)):
-                        tableDict[tableKey[elementNum]] = tableValue[elementNum]
-                    table.append(tableDict)
-            wholeTable.extend(table)
+                    wholeTable[tableRecordKey] = [tableRecordValue,]
         return wholeTable
 
 
 if __name__ == '__main__':
     print os.path.dirname(os.path.realpath(__file__))
-    table = TableReader()
-    loanTableList = table.readTable([os.path.join(os.path.dirname(os.path.realpath(__file__)),'2014-02-28.txt',)])
-    for dict in loanTableList:
-        for key in dict:
-            print key,dict[key]
+    table = ContactReader([os.path.join(os.path.dirname(os.path.realpath(__file__)),'sighedTable.txt',)], 'custCode')
+    loanTableList = table.read()
+    print loanTableList
