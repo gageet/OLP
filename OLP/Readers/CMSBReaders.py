91fc95fe6c9769a2c1f133f5d61c6e994c478c25
plumiron <plumiron@gmail.com>
2015-07-22 20:22:41 +0800

    add CMSBTransReader & update testCMSBReaders.py [lk]

--- OLP/Readers/CMSBReaders.py
+++ OLP/Readers/CMSBReaders.py
@@ -1,9 +1,12 @@
 # coding: utf-8
+
 from OLP.Readers.Reader import Reader
 
 
 class CMSBLoanReader(Reader):
-
+    '''
+    贷款数据读取类
+    '''
     def __init__(self, filenames, primKey):
         self.filenames = filenames
         self.primKey = primKey
@@ -41,3 +44,44 @@ class CMSBLoanReader(Reader):
                         loan[j][i] = val
 
         return title2index, loans
+
+
+class CMSBTransReader(Reader):
+    '''
+    流水数据读取类
+    '''
+    def __init__(self, filenames, primKey):
+        self.filenames = filenames
+        self.primKey = primKey
+
+    def read(self):
+        filenames = self.filenames
+        title2index = {}
+        transs = {}
+
+        # 遍历nMonth月数据
+        for i, filename in enumerate(filenames):
+            inFile = open(filename)
+
+            # 读取第一行，获取字段名
+            firstLine = inFile.readline()
+            titles = firstLine.strip().split('\t')
+            for index, title in enumerate(titles):
+                title2index[title] = index
+
+            # 读取剩余行，获取各字段对应数据
+            for line in inFile:
+                vals = line.strip().split('\t')
+                primKeyIndex = title2index[self.primKey]
+                primKeyVal = vals[primKeyIndex]
+                if primKeyVal not in transs:
+                    trans = []
+                    for val in vals:
+                        trans.append([val])
+                    transs[primKeyVal] = trans
+                else:
+                    trans = transs[primKeyVal]
+                    for j, val in enumerate(vals):
+                        trans[j].append(val)
+
+        return title2index, transs
