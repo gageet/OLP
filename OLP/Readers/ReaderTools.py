cd52c75bda5651e8ecdf10bf9520c10512bdb29c
i405817461 <likeliurui@qq.com>
2015-07-24 15:13:53 +0800

    add UniPrinter in ReaderTools.py

--- OLP/Readers/ReaderTools.py
+++ OLP/Readers/ReaderTools.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from datetime import datetime
+import pprint, cStringIO
 
 
 class TimeTools:
@@ -11,3 +12,36 @@ class TimeTools:
 
     def str2Date(self, str, separator):
         return datetime.strptime(str, "%Y"+separator+"%m"+separator+"%d")
+
+
+class UniPrinter(pprint.PrettyPrinter):
+    '''
+    工具类，打印字典，列表或几个容器嵌套且包含中文的情况。调用格式：UniPrinter().pprint(Object)
+    '''
+    def format(self, obj, context, maxlevels, level):
+        if isinstance(obj, unicode):
+            out = cStringIO.StringIO()
+            out.write('u"')
+            for c in obj:
+                if ord(c)<32 or c in u'"\\':
+                    out.write('\\x%.2x' % ord(c))
+                else:
+                    out.write(c.encode("utf-8"))
+
+            out.write('"')
+            # result, readable, recursive
+            return out.getvalue(), True, False
+        elif isinstance(obj, str):
+            out = cStringIO.StringIO()
+            out.write('"')
+            for c in obj:
+                if ord(c)<32 or c in '"\\':
+                    out.write('\\x%.2x' % ord(c))
+                else:
+                    out.write(c)
+
+            out.write('"')
+            # result, readable, recursive
+            return out.getvalue(), True, False
+        else:
+            return pprint.PrettyPrinter.format(self, obj, context, maxlevels, level)
