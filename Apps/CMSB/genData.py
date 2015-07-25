3290ad6616418b7afd9f1ef1d79c23bf567e8b77
plumiron <plumiron@gmail.com>
2015-07-25 19:14:52 +0800

    update

--- Apps/CMSB/genData.py
+++ Apps/CMSB/genData.py
@@ -62,7 +62,7 @@ def genRandList(n):
     return list(elts)
 
 
-fieldNames2Generator = {
+loanFieldNames2Generator = {
     # '协议号': getRandIntGenerator(100000000, 800000000),
     '协议修饰符': getRandEltGenerator(genRandList(2)),
     '币种': getRandEltGenerator(genRandList(2)),
@@ -191,22 +191,116 @@ fieldNames2Generator = {
 }
 
 
-def genLoans(nSamples, filename):
+transFieldNames2Generator = {
+    '业务标识': getRandEltGenerator(genRandList(2)),
+    '核心主键': getRandStrGenerator(15),
+    '流水号': getRandStrGenerator(15),
+    '传票号': getRandStrGenerator(15),
+    '交易机构': getRandEltGenerator(genRandList(2)),
+    '交易日期': getRandDateGenerator(),
+    '交易时间': getRandDateGenerator(),
+    '我行账户': getRandStrGenerator(15),
+    # '我行客户号': getRandStrGenerator(15),
+    '客户类型': getRandEltGenerator(genRandList(2)),
+    '交易代码': getRandEltGenerator(genRandList(2)),
+    '原交易代码': getRandEltGenerator(genRandList(2)),
+    '业务类型': getRandEltGenerator(genRandList(2)),
+    '交易类型': getRandEltGenerator(genRandList(2)),
+    '借贷标志': getRandBoolGenerator(),
+    '收付标志': getRandBoolGenerator(),
+    '科目号': getRandEltGenerator(genRandList(2)),
+    '币种': getRandEltGenerator(genRandList(2)),
+    '本外币标志': getRandBoolGenerator(),
+    '原币交易金额': getRandFltGenerator(1000, 1000000),
+    '折人民币': getRandFltGenerator(1000, 1000000),
+    '折美元': getRandFltGenerator(1000, 1000000),
+    '余额': getRandFltGenerator(1000, 1000000),
+    '现/转标志': getRandBoolGenerator(),
+    '汇款标志': getRandBoolGenerator(),
+    '摘要': getRandStrGenerator(15),
+    '是否跨境交易': getRandBoolGenerator(),
+    '结算方式': getRandEltGenerator(genRandList(2)),
+    '用途': getRandStrGenerator(15),
+    '对方系统': getRandEltGenerator(genRandList(2)),
+    '对方是否我行客户': getRandBoolGenerator(),
+    '对方所在地区': getRandEltGenerator(genRandList(2)),
+    '对方行号类型': getRandEltGenerator(genRandList(2)),
+    '对方行号': getRandStrGenerator(15),
+    '对方银行名称': getRandStrGenerator(15),
+    '对方客户号': getRandStrGenerator(15),
+    '对方名称': getRandStrGenerator(15),
+    '对方账号': getRandStrGenerator(15),
+    '对方交易日期': getRandDateGenerator(),
+    '对方账户类型': getRandEltGenerator(genRandList(2)),
+    '对方证件类型': getRandEltGenerator(genRandList(2)),
+    '对方证件号码': getRandStrGenerator(15),
+    '对方客户类型': getRandEltGenerator(genRandList(2)),
+    '抹账标志': getRandBoolGenerator(),
+    '发生标志': getRandBoolGenerator(),
+    '批量标志': getRandBoolGenerator(),
+    '柜员': getRandStrGenerator(15),
+    '是否需补录': getRandBoolGenerator(),
+    '处理状态': getRandEltGenerator(genRandList(2)),
+    '当事人中文名称': getRandStrGenerator(15),
+    '是否已补录': getRandBoolGenerator(),
+    '补录时间': getRandDateGenerator(),
+    '对方卡号': getRandStrGenerator(15),
+    '凭证代码': getRandEltGenerator(genRandList(2)),
+    '原客户号': getRandStrGenerator(15),
+    '交易渠道': getRandEltGenerator(genRandList(2)),
+    '是否计算': getRandBoolGenerator(),
+    '是否规则': getRandBoolGenerator(),
+    '事件类型': getRandEltGenerator(genRandList(2)),
+    '对方金融机构国家': getRandEltGenerator(genRandList(2)),
+    '网银IP地址': getRandStrGenerator(15),
+    '交易去向国别': getRandEltGenerator(genRandList(2)),
+    '交易去向行政区': getRandEltGenerator(genRandList(2)),
+    '交易发生地国别': getRandEltGenerator(genRandList(2)),
+    '交易发生地行政区': getRandEltGenerator(genRandList(2)),
+    '代办人姓名': getRandStrGenerator(15),
+    '代办人身份证件/证明文件类型': getRandEltGenerator(genRandList(2)),
+    '代办人身份证件/证明文件号码': getRandStrGenerator(15),
+    '代办人国籍': getRandEltGenerator(genRandList(2)),
+    '金融机构和交易关系': getRandEltGenerator(genRandList(2)),
+    '现钞标志': getRandBoolGenerator(),
+    '对方人行客户类型': getRandEltGenerator(genRandList(2)),
+    '交易卡号': getRandStrGenerator(15),
+    '上次更新柜员': getRandStrGenerator(15),
+    '对方是否离岸账户': getRandBoolGenerator(),
+    '业务代号': getRandStrGenerator(15),
+    '大额验证状态': getRandEltGenerator(genRandList(2)),
+    '可疑验证状态': getRandEltGenerator(genRandList(2)),
+    'ETL业务日期': getRandDateGenerator(),
+}
+
+
+def genData(nSamples, loanFilename, transFilename):
     protolNums = ['%d' % i for i in range(1, nSamples + 1)]
     custNums = ['%d' % i for i in range(1, nSamples + 1)]
 
-    with open(filename, 'w') as outFile:
-        fieldNames = fieldNames2Generator.keys()
-        outFile.write('\t'.join(['协议号', '核心客户号'] + fieldNames) + '\n')
+    with open(loanFilename, 'w') as loanFile:
+        fieldNames = loanFieldNames2Generator.keys()
+        loanFile.write('\t'.join(['协议号', '核心客户号'] + fieldNames) + '\n')
         for protolNum, custNum in zip(protolNums, custNums):
-            outFile.write('%s\t%s' % (protolNum, custNum))
+            loanFile.write('%s\t%s' % (protolNum, custNum))
             for fieldName in fieldNames:
-                field = fieldNames2Generator[fieldName]()
-                outFile.write('\t%s' % str(field))
-            outFile.write('\n')
+                field = loanFieldNames2Generator[fieldName]()
+                loanFile.write('\t%s' % str(field))
+            loanFile.write('\n')
 
+    with open(transFilename, 'w') as transFile:
+        fieldNames = transFieldNames2Generator.keys()
+        transFile.write('\t'.join(['我行客户号'] + fieldNames) + '\n')
+        for custNum in custNums:
+            for transNum in range(random.randint(1, 10)):
+                transFile.write('%s' % (custNum))
+                for fieldName in fieldNames:
+                    field = transFieldNames2Generator[fieldName]()
+                    transFile.write('\t%s' % str(field))
+                transFile.write('\n')
 
 if __name__ == '__main__':
     nSamples = int(sys.argv[1])
-    filename = sys.argv[2]
-    genLoans(nSamples, filename)
+    loanFilename = sys.argv[2]
+    transFilename = sys.argv[3]
+    genData(nSamples, loanFilename, transFilename)
