3caa4368accf76f7e683f583f2c4e6e22b4171ad
plumiron <plumiron@gmail.com>
2015-07-27 10:33:22 +0800

    update CMSBReaders.py [lk]

--- Apps/CMSB/run.py
+++ Apps/CMSB/run.py
@@ -31,17 +31,17 @@ import config as cf
 #reader.read()
 
 reader = CMSBReader(cf.fieldName2fieldType)
-loanFieldName2Index, loans = reader.readLoans('/home/lk/Bank/Data/Loans/2014-2')
+loanFieldName2Index, loans = reader.readLoans(['/home/lk/Bank/Data/Loans/2014-2', '/home/lk/Bank/Data/Loans/2014-3'])
 for loan in loans.values():
     for name, index in loanFieldName2Index.items():
         print '%s:%s' % (name, str(loan[index])),
     print '\n', '-' * 20
-transFieldName2Index, transs = reader.readTranss('/home/lk/Bank/Data/Transactions/2014-2')
+transFieldName2Index, transs = reader.readTranss(['/home/lk/Bank/Data/Transactions/2014-2', '/home/lk/Bank/Data/Transactions/2014-3'])
 for trans in transs.values():
     for name, index in transFieldName2Index.items():
         print '%s:%s' % (name, str(trans[index])),
     print '\n', '-' * 20
-prodFieldName2Index, prods = reader.readProds('/home/lk/Bank/Data/Products/2014-2')
+prodFieldName2Index, prods = reader.readProds(['/home/lk/Bank/Data/Products/2014-2'])
 for prod in prods.values():
     for name, index in prodFieldName2Index.items():
         print '%s:%s' % (name, str(prod[index])),
--- OLP/Readers/CMSBReaders.py
+++ OLP/Readers/CMSBReaders.py
@@ -79,56 +79,62 @@ class CMSBReader(Reader):
         self.protolNumName = '协议号'
         self.custNumName = '我行客户号'
 
-    def readLoans(self, filename):
+    def readLoans(self, filenames):
         fieldName2Index = {}
         loans = {}
 
-        with open(filename) as inFile:
-            # 读取第一行
-            firstLine = inFile.readline()
-            fieldNames = firstLine.strip().split('\t')  # 利用第一行获取所有字段
-            fieldName2Index = self.fieldProcessor.buildLoanFieldNameIndex(fieldNames)  # 构建字段索引
-            # 读取剩余行，获取各字段对应数据
-            for line in inFile:
-                fields = line.strip().split('\t')
-                fields = self.fieldProcessor.convertLoanFields(fields)
-                self.fieldProcessor.addLoanFields(fields, loans, self.protolNumName)
-            inFile.close()
+        for i, filename in enumerate(filenames):
+            with open(filename) as inFile:
+                # 读取第一行
+                firstLine = inFile.readline()
+                if i == 0:  # 第一次读取文件
+                    fieldNames = firstLine.strip().split('\t')  # 利用第一行获取所有字段
+                    fieldName2Index = self.fieldProcessor.buildLoanFieldNameIndex(fieldNames)  # 构建字段索引
+                # 读取剩余行，获取各字段对应数据
+                for line in inFile:
+                    fields = line.strip().split('\t')
+                    fields = self.fieldProcessor.convertLoanFields(fields)
+                    self.fieldProcessor.addLoanFields(fields, loans, self.protolNumName)
+                inFile.close()
 
         return fieldName2Index, loans
 
-    def readTranss(self, filename):
+    def readTranss(self, filenames):
         fieldName2Index = {}
         transs = {}
 
-        with open(filename) as inFile:
-            # 读取第一行
-            firstLine = inFile.readline()
-            fieldNames = firstLine.strip().split('\t')  # 第一次读取，利用第一行获取所有字段
-            fieldName2Index = self.fieldProcessor.buildTransFieldNameIndex(fieldNames)  # 构建字段索引
-            # 读取剩余行，获取各字段对应数据
-            for line in inFile:
-                fields = line.strip().split('\t')
-                fields = self.fieldProcessor.convertTransFields(fields)
-                self.fieldProcessor.addTransFields(fields, transs, self.custNumName)
-            inFile.close()
+        for i, filename in enumerate(filenames):
+            with open(filename) as inFile:
+                # 读取第一行
+                firstLine = inFile.readline()
+                if i == 0:  # 第一次读取文件
+                    fieldNames = firstLine.strip().split('\t')  # 第一次读取，利用第一行获取所有字段
+                    fieldName2Index = self.fieldProcessor.buildTransFieldNameIndex(fieldNames)  # 构建字段索引
+                # 读取剩余行，获取各字段对应数据
+                for line in inFile:
+                    fields = line.strip().split('\t')
+                    fields = self.fieldProcessor.convertTransFields(fields)
+                    self.fieldProcessor.addTransFields(fields, transs, self.custNumName)
+                inFile.close()
 
         return fieldName2Index, transs
 
-    def readProds(self, filename):
+    def readProds(self, filenames):
         fieldName2Index = {}
         prods = {}
 
-        with open(filename) as inFile:
-            # 读取第一行
-            firstLine = inFile.readline()
-            fieldNames = firstLine.strip().split('\t')  # 第一次读取，利用第一行获取所有字段
-            fieldName2Index = self.fieldProcessor.buildProdFieldNameIndex(fieldNames)  # 构建字段索引
-            # 读取剩余行，获取各字段对应数据
-            for line in inFile:
-                fields = line.strip().split('\t')
-                fields = self.fieldProcessor.convertProdFields(fields)
-                self.fieldProcessor.addProdFields(fields, prods, self.custNumName)
-            inFile.close()
+        for i, filename in enumerate(filenames):
+            with open(filename) as inFile:
+                # 读取第一行
+                firstLine = inFile.readline()
+                if i == 0:  # 第一次读取文件
+                    fieldNames = firstLine.strip().split('\t')  # 第一次读取，利用第一行获取所有字段
+                    fieldName2Index = self.fieldProcessor.buildProdFieldNameIndex(fieldNames)  # 构建字段索引
+                # 读取剩余行，获取各字段对应数据
+                for line in inFile:
+                    fields = line.strip().split('\t')
+                    fields = self.fieldProcessor.convertProdFields(fields)
+                    self.fieldProcessor.addProdFields(fields, prods, self.custNumName)
+                inFile.close()
 
         return fieldName2Index, prods
