261dda1b785b0ac9d4a584b79d114cecda054433
plumiron <plumiron@gmail.com>
2015-07-27 19:37:32 +0800

    update Metric.py & ClassificationMetrics.py

--- OLP/Metrics/ClassificationMetrics.py
+++ OLP/Metrics/ClassificationMetrics.py
@@ -1,25 +1,37 @@
+from OLP.Metrics.Metric import FltMetric
 from sklearn import metrics
 
 
-def getAccuracy(ysTrue, ysPred):
-    return metrics.accuracy_score(ysTrue, ysPred)
+class Accuracy(FltMetric):
+
+    @classmethod
+    def get(cls, ysTrue, ysPred):
+        return metrics.accuracy_score(ysTrue, ysPred)
 
 
-def getPrecision(ysTrue, ysPred):
-    return metrics.precision_score(ysTrue, ysPred)
+class Precision(FltMetric):
+
+    @classmethod
+    def get(cls, ysTrue, ysPred):
+        return metrics.precision_score(ysTrue, ysPred)
 
 
-def getRecall(ysTrue, ysPred):
-    return metrics.recall_score(ysTrue, ysPred)
+class Recall(FltMetric):
+
+    @classmethod
+    def get(cls, ysTrue, ysPred):
+        return metrics.recall_score(ysTrue, ysPred)
 
 
-def getF1(ysTrue, ysPred):
-    return metrics.f1_score(ysTrue, ysPred)
+class F1(FltMetric):
+
+    @classmethod
+    def get(cls, ysTrue, ysPred):
+        return metrics.f1_score(ysTrue, ysPred)
 
 
-def getAUC(ysTrue, ysPred):
-    return metrics.roc_auc_score(ysTrue, ysPred)
+class AUC(FltMetric):
 
-
-def getConfusionMatrix(ysTrue, ysPred):
-    return metrics.confusion_matrix(ysTrue, ysPred)
+    @classmethod
+    def get(cls, ysTrue, ysPred):
+        return metrics.roc_auc_score(ysTrue, ysPred)
