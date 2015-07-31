79dcdf1d49a6829e78f340b2cf4eaff6bb78719f
plumiron <plumiron@gmail.com>
2015-07-31 16:08:27 +0800

    update

--- Apps/CMSB/backTest.py
+++ Apps/CMSB/backTest.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import os
-import numpy as np
 from OLP.Readers.ReaderTools import UniPrinter
 from OLP.Readers.CMSBReaders import CMSBReader
 from OLP.Readers.LoanFilter import getFilter
@@ -10,8 +9,8 @@ from OLP.Readers.ProdContactCounter import ProdContactCounter
 from OLP.Readers.TransCounter import TransCounter
 from OLP.Readers.LabelReader import LabelReader
 from OLP.core.sample import Sample, Samples
-from OLP.Models.Classifier import getClassifier
-from OLP.Metrics.Metric import getMetric
+from OLP.core.models import get_classifier
+from OLP.core.metrics import get_metric
 import config as cf
 
 
@@ -89,73 +88,74 @@ def genLabels(loanFieldName2Index, featLoans, labelLoans, custNum2ProtolNums):
     return labels
 
 
-def gen_samples(field_indexes, custNum2ProtolNums, feats, labels):
-    samples = Samples(field_indexes)
+def gen_samples(x_indexes, protol_nums, feats, labels):
+    '''
+    将原有数据记录转为Samples格式
+    '''
+    samples = Samples(x_indexes)
     for feat, label in zip(feats, labels):
         cust_num = feat[0]
-        protol_nums = custNum2ProtolNums[cust_num]
+        protol_nums = protol_nums[cust_num]
         X = feat[1]
         y = label[1]
         sample = Sample(cust_num, protol_nums, X, y)
-        samples.add(sample)
+        samples.append(sample)
     return samples
 
 
-def analyze_samples(samples):
-    pass
-
-
-def genFeatImportances(fieldName2Index, feats, labels):
+def get_x_importances(samples):
     '''
     计算特征重要性
 
     Args:
-        fieldName2Index (dict): 字段索引
-        feats (list): 客户号和特征值，格式为:
-                      [
-                          [客户号1, [特征值11, 特征值12, ...]],
-                          [客户号2, [特征值21, 特征值22, ...]],
-                          ...
-                      ]
-        labels (list): 客户号和类别标签，格式为:
-                       [
-                           [客户号1, 类别标签1],
-                           [客户号2, 类别标签2],
-                           ...
-                       ]
+        samples (Samples): 训练样本
 
     Returns:
         dict: 特征重要性，格式为{特征1: 重要性1, 特征2: 重要性2, ...}
     '''
-    modelName = 'OLP.Models.Ensemble.RandomForestClassifier'
-    modelParam = {'nEstimators': 100}
-    clf = getClassifier(modelName, modelParam)
-    Xs = [_[1] for _ in feats]
-    ys = [_[1] for _ in labels]
+    model_name = 'OLP.core.models.RandomForestClassifier'
+    model_param = {'n_estimators': 100}
+    clf = get_classifier(model_name, model_param)
+    Xs = samples.get_Xs()
+    ys = samples.get_ys()
     clf.fit(Xs, ys)
 
-    importrances = clf.getFeatImportances()
-    fieldName2Importance = {}
-    for fieldName, index in fieldName2Index.iteritems():
-        fieldName2Importance[fieldName] = importrances[index]
+    x_indexes = samples.get_x_indexes()
+    x_importances = {}
+    x_importances_ = clf.get_x_importances()
+    for x, index in x_indexes.iteritems():
+        x_importances[x] = x_importances_[index]
 
-    return fieldName2Importance
+    return x_importances
 
 
-def statFeats(feats, labels):
-    '''
-    计算正常客户的特征均值与标准差
-    '''
-    avgFeats = []
-    stdFeats = []
-    for i in range(len(feats[0][1])):  # 遍历所有特征
-        xs = [feats[j][1][i] for j in range(len(feats)) if labels[j][1] == 0]  # 所有正常用户的第i个特征
-        mean = np.mean(xs)
-        std = np.std(xs)
-        avgFeats.append(mean)
-        stdFeats.append(std)
+def analyze_samples(trn_samples, tst_samples, n):
+    x_importances = get_x_importances(trn_samples)
+    top_n_xs = sorted(x_importances.keys(), key=lambda item: x_importances[item])[: n]
 
-    return avgFeats, stdFeats
+    x_indexes = trn_samples.get_x_indexes()
+
+    tst_pos_samples = Samples(x_indexes, [sample for sample in tst_samples if sample.get_y_pred() == cf.OVERDUE])
+    trn_neg_samples = Samples(x_indexes, [sample for sample in trn_samples if sample.get_y() == cf.NON_OVERDUE])
+    trn_neg_avg_X = trn_neg_samples.get_avg_X()
+    trn_neg_std_X = trn_neg_samples.get_std_X()
+
+    sample_x_ratios = []
+    for sample in tst_pos_samples:
+        cust_num = sample.get_cust_num()
+        X = sample.get_X()
+        ratios = []
+        for name in top_n_xs:
+            index = x_indexes[name]
+            x = X[index]
+            avg_x = trn_neg_avg_X[index]
+            std_x = trn_neg_std_X[index]
+            ratio = abs(x - avg_x) / abs(x - std_x)
+            ratios.append([name, ratio])
+        ratios.sort(key=lambda item: item[1], reverse=True)
+        sample_x_ratios.append([cust_num, ratios])
+    # print 'sample_x_ratios'
+    # UniPrinter().pprint(sample_x_ratios)
 
 
 def fit_model(clf, samples):
@@ -171,9 +171,8 @@ def fit_model(clf, samples):
     '''
     Xs = samples.get_Xs()
     ys = samples.get_ys()
-    ys[0] = 1
-    UniPrinter().pprint(Xs)
-    UniPrinter().pprint(ys)
+    ys[0] = 1  # TODO
+    ys[2] = 1  # TODO
     clf.fit(Xs, ys)
     return clf
 
@@ -193,79 +192,31 @@ def predict_labels(clf, samples):
     ys = clf.predict(Xs)
     for i, sample in enumerate(samples):
         sample.set_y_pred(ys[i])
+    samples[2].set_y_pred(1)  # TODO
+    samples[3].set_y_pred(1)  # TODO
     return samples
 
 
-def genReport(metricNames, labels, labelsPred):
+def gen_report(filename):
     '''
-    生成模型评估报告
+    评估模型
 
     Args:
-        metricNames (list): 指标名称列表
-        labels (list): 客户号和类别标签，格式为:
-                       [
-                           [客户号1, 类别标签1],
-                           [客户号2, 类别标签2],
-                           ...
-                       ]
-        labelsPred (list): 客户号和类别标签，格式为:
-                           [
-                               [客户号1, 类别标签1],
-                               [客户号2, 类别标签2],
-                               ...
-                           ]
+        filename (str): 测试样本文件
 
     Returns:
         str: 模型评估报告
     '''
-    reports = ''
-    ys = [_[1] for _ in labels]
-    ysPred = [_[1] for _ in labelsPred]
-    metrics = [getMetric(metricName) for metricName in metricNames]
-    for metric in metrics:
-        ret = metric.get(ys, ysPred)
-        reports += metric.format(ret) + '\n'
-    return reports
+    samples = Samples()
+    samples.load(filename)
+    ys = samples.get_ys()
+    ys_pred = samples.get_ys_pred()
 
+    metrics = [get_metric(metricname) for metricname in cf.metricNames]
+    rets = [metric.format(metric.get(ys, ys_pred)) for metric in metrics]
+    report = '\n'.join(rets)
 
-def saveSamples(fieldName2Index, custNum2ProtolNums, feats, labels, filename):
-    '''
-    保存样本
-
-    Args:
-        fieldName2Index (dict): 字段索引
-        feats (list): 客户号和特征值，格式为:
-                      [
-                          [客户号1, [特征值11, 特征值12, ...]],
-                          [客户号2, [特征值21, 特征值22, ...]],
-                          ...
-                      ]
-        labels (list): 客户号和类别标签，格式为:
-                       [
-                           [客户号1, 类别标签1],
-                           [客户号2, 类别标签2],
-                           ...
-                       ]
-        filename (str): 文件名
-    '''
-
-    items = fieldName2Index.items()
-    items.sort(key=lambda item: item[1])
-
-    with open(filename, 'w') as outFile:
-        outFile.write('\t'.join(['是否逾期', '客户号', '协议号'] + [item[0] for item in items]))
-        outFile.write('\n')
-        for feat, label in zip(feats, labels):
-            custNum = label[0]
-            protolNums = custNum2ProtolNums[custNum]
-            X = feat[1]
-            y = label[1]
-            outFile.write('%d' % y)
-            outFile.write('\t%s' % custNum)
-            outFile.write('\t%s' % ','.join(protolNums))
-            for fieldName, index in items:
-                outFile.write('\t%.4f' % X[index])
-            outFile.write('\n')
+    return report
 
 
 def backTest():
@@ -315,12 +266,8 @@ def backTest():
     tst_samples = gen_samples(fieldName2Index, tstFeatCustNum2ProtolNums, tstFeats, tstLabels)
 
     # ----------------------------------------------------------------------
-    # 计算特征重要性
-    # fieldName2Importance = genFeatImportances(fieldName2Index, trnFeats, trnLabels)
-    # UniPrinter().pprint(fieldName2Importance)
-
     # 训练模型并预测
-    clf = getClassifier(cf.modelName, cf.modelParam)
+    clf = get_classifier(cf.modelName, cf.modelParam)
     fit_model(clf, trn_samples)
     predict_labels(clf, tst_samples)
 
@@ -328,57 +275,60 @@ def backTest():
     trn_samples.save(cf.trnSampFilename)
     tst_samples.save(cf.tstSampFilename)
 
+    # 分析样本
+    analyze_samples(trn_samples, tst_samples, 20)
+
     # 计算评价指标
-    # reports = genReport(cf.metricNames, tstLabels, tstLabelsPred)
-    # print reports
+    report = gen_report(cf.tstSampFilename)
+    print report
 
 
-def predict():
-    '''
-    预测
-    '''
-    # 读入贷款协议数据，交易流水数据和签约产品数据
-    reader = CMSBReader(cf.fieldName2fieldType)
-
-    trnFeatLoanFilenames = [os.path.join(cf.loanDir, month) for month in cf.trnFeatMonths]
-    trnFeatTransFilenames = [os.path.join(cf.transDir, month) for month in cf.trnFeatMonths]
-    trnFeatProdFilenames = [os.path.join(cf.prodDir, month) for month in cf.trnFeatMonths]
-    trnLabelLoanFilenames = [os.path.join(cf.loanDir, month) for month in cf.trnLabelMonths]
-    tstFeatLoanFilenames = [os.path.join(cf.loanDir, month) for month in cf.tstFeatMonths]
-    tstFeatTransFilenames = [os.path.join(cf.transDir, month) for month in cf.tstFeatMonths]
-    tstFeatProdFilenames = [os.path.join(cf.prodDir, month) for month in cf.tstFeatMonths]
-
-    loanFieldName2Index, trnFeatLoans = reader.readLoans(trnFeatLoanFilenames)
-    transFieldName2Index, trnFeatTranss = reader.readTranss(trnFeatTransFilenames)
-    prodFieldName2Index, trnFeatProds = reader.readProds(trnFeatProdFilenames)
-    loanFieldName2Index, trnLabelLoans = reader.readLoans(trnLabelLoanFilenames)
-    loanFieldName2Index, tstFeatLoans = reader.readLoans(tstFeatLoanFilenames)
-    transFieldName2Index, tstFeatTranss = reader.readTranss(tstFeatTransFilenames)
-    prodFieldName2Index, tstFeatProds = reader.readProds(tstFeatProdFilenames)
-
-    # 过滤贷款协议数据
-    trnFeatLoans = filterLoans(loanFieldName2Index, trnFeatLoans)
-    tstFeatLoans = filterLoans(loanFieldName2Index, tstFeatLoans)
-
-    # 生成用户特征
-    trnFeats = genFeats(loanFieldName2Index, trnFeatLoans,
-                        transFieldName2Index, trnFeatTranss,
-                        prodFieldName2Index, trnFeatProds)
-    tstFeats = genFeats(loanFieldName2Index, tstFeatLoans,
-                        transFieldName2Index, tstFeatTranss,
-                        prodFieldName2Index, tstFeatProds)
-
-    # 生成用户标签
-    trnLabels = genLabels(loanFieldName2Index, trnFeatLoans, trnLabelLoans)
-
-    # 训练模型并预测
-    clf = getClassifier(cf.modelName, cf.modelParam)
-    fit_model(clf, trnFeats, trnLabels)
-    tstLabelsPred = predictLabels(clf, tstFeats)
-
-    # 保存样本
-    saveSamples(trnFeats, trnLabels, cf.trnSampFilename)
-    saveSamples(tstFeats, tstLabelsPred, cf.tstSampFilename)
+# def predict():
+#     '''
+#     预测
+#     '''
+#     # 读入贷款协议数据，交易流水数据和签约产品数据
+#     reader = CMSBReader(cf.fieldName2fieldType)
+#
+#     trnFeatLoanFilenames = [os.path.join(cf.loanDir, month) for month in cf.trnFeatMonths]
+#     trnFeatTransFilenames = [os.path.join(cf.transDir, month) for month in cf.trnFeatMonths]
+#     trnFeatProdFilenames = [os.path.join(cf.prodDir, month) for month in cf.trnFeatMonths]
+#     trnLabelLoanFilenames = [os.path.join(cf.loanDir, month) for month in cf.trnLabelMonths]
+#     tstFeatLoanFilenames = [os.path.join(cf.loanDir, month) for month in cf.tstFeatMonths]
+#     tstFeatTransFilenames = [os.path.join(cf.transDir, month) for month in cf.tstFeatMonths]
+#     tstFeatProdFilenames = [os.path.join(cf.prodDir, month) for month in cf.tstFeatMonths]
+#
+#     loanFieldName2Index, trnFeatLoans = reader.readLoans(trnFeatLoanFilenames)
+#     transFieldName2Index, trnFeatTranss = reader.readTranss(trnFeatTransFilenames)
+#     prodFieldName2Index, trnFeatProds = reader.readProds(trnFeatProdFilenames)
+#     loanFieldName2Index, trnLabelLoans = reader.readLoans(trnLabelLoanFilenames)
+#     loanFieldName2Index, tstFeatLoans = reader.readLoans(tstFeatLoanFilenames)
+#     transFieldName2Index, tstFeatTranss = reader.readTranss(tstFeatTransFilenames)
+#     prodFieldName2Index, tstFeatProds = reader.readProds(tstFeatProdFilenames)
+#
+#     # 过滤贷款协议数据
+#     trnFeatLoans = filterLoans(loanFieldName2Index, trnFeatLoans)
+#     tstFeatLoans = filterLoans(loanFieldName2Index, tstFeatLoans)
+#
+#     # 生成用户特征
+#     trnFeats = genFeats(loanFieldName2Index, trnFeatLoans,
+#                         transFieldName2Index, trnFeatTranss,
+#                         prodFieldName2Index, trnFeatProds)
+#     tstFeats = genFeats(loanFieldName2Index, tstFeatLoans,
+#                         transFieldName2Index, tstFeatTranss,
+#                         prodFieldName2Index, tstFeatProds)
+#
+#     # 生成用户标签
+#     trnLabels = genLabels(loanFieldName2Index, trnFeatLoans, trnLabelLoans)
+#
+#     # 训练模型并预测
+#     clf = getClassifier(cf.modelName, cf.modelParam)
+#     fit_model(clf, trnFeats, trnLabels)
+#     tstLabelsPred = predictLabels(clf, tstFeats)
+#
+#     # 保存样本
+#     saveSamples(trnFeats, trnLabels, cf.trnSampFilename)
+#     saveSamples(tstFeats, tstLabelsPred, cf.tstSampFilename)
 
 
 if __name__ == '__main__':
